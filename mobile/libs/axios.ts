d0de12d9e251f3bde75771f8f233acda107733e7
TanmayAggarwal87 <tanmayagg.2005@gmail.com>
2025-08-30 15:56:26 +0530

    Fixes

--- mobile/libs/axios.ts
+++ mobile/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.29.59:4000",
+  baseURL: "http://192.168.x.x:4000",
   withCredentials: true,
 
 });
